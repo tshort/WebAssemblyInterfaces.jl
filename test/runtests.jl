6829e27304dcf89bd56a21879d1cf9613fb1daab
Tom Short <tshort.rlists@gmail.com>
2023-06-03 20:22:53 -0400

    Tweak test

--- test/runtests.jl
+++ test/runtests.jl
@@ -48,7 +48,7 @@ c: 3,
 
 s = js_repr(ones(3,2))
 
-@test contains(s, "new ffi.julia.Array64('f64', [3, 2], [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, ])")
+@test contains(s, string("new ffi.julia.Array", sizeof(Int) * 8, "('f64', [3, 2], [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, ])"))
 
 
 # More sophisticated tests could use NodeJS.jl to run some JavaScript/WebAssembly.
